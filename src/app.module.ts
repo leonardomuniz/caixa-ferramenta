34a11254820d1d1dd7916d3d5a5a0b8b32919b5b
leonardomuniz <leocontato@outlook.com.br>
2026-04-26 21:04:59 -0300

    feat: migra cache in-memory para Redis global com ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,30 @@ import { ChinenigansModule } from './chinenigans/chinenigans.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './config/logging.interceptor';
 import { HttpExceptionFilter } from './config/httpException.filter';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
 
 @Module({
-  imports: [ChinenigansModule],
+  imports: [
+    ChinenigansModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        stores: [createKeyv(config.get<string>('REDIS_URL'))],
+        ttl: 60_000,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [
     { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
-    AppService
+    AppService,
   ],
 })
-export class AppModule { }
+export class AppModule {}
--- src/chinenigans/chinenigans.module.ts
+++ src/chinenigans/chinenigans.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChinenigansService } from './chinenigans.service';
 import { ChinenigansController } from './chinenigans.controller';
-import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [CacheModule.register()],
   controllers: [ChinenigansController],
   providers: [ChinenigansService],
 })
-export class ChinenigansModule { }
+export class ChinenigansModule {}
